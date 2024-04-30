d0b2ca7cb54e8280a6f21099622a1f8aafae2d0f
Yongjun Zhang <emailyongjunzhang@gmail.com>
2024-04-30 16:55:55 +0800

    feature: add password generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use anyhow::Ok;
 // rcli csv -i input.csv -o output.json --header -d ','
 use clap::Parser;
-use rcli::{process_csv, Opts, SubCommand};
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
@@ -12,6 +13,15 @@ fn main() -> anyhow::Result<()> {
                 format!("output.{}", opts.format)
             };
             process_csv(&opts.input, output, opts.format)?;
+        }
+        SubCommand::GenPass(opts) => {
+            process_genpass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.number,
+                opts.symbol,
+            )?;
         }
     }
     Ok(())
